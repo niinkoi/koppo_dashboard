76746c3ca4d45816e849ece1d174a78faaa452f9
koiniin <nkhoinguyen26@gmail.com>
2022-06-07 16:59:48 +0700

    Lint UT

--- spec/controllers/api/v1/session_controller_spec.rb
+++ spec/controllers/api/v1/session_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe Api::V1::SessionController do
   describe '#new_session' do
-
     let(:request_params) do
       {
         email: 'admin123@admin.com',
@@ -15,14 +14,14 @@ describe Api::V1::SessionController do
         post :new_session, params: request_params.except(:email)
         expect(response.status).eql?(400)
         expect(JSON.parse(response.body))
-          .to eq({ "errors" => [ "Missing required parameter 'email'" ] })
+          .to eq({ 'errors' => ["Missing required parameter 'email'"] })
       end
 
       it 'return 400 when password is missing' do
         post :new_session, params: request_params.except(:password)
         expect(response.status).eql? 400
         expect(JSON.parse(response.body))
-          .to eq({ "errors" => [ "Missing required parameter 'password'" ] })
+          .to eq({ 'errors' => ["Missing required parameter 'password'"] })
       end
     end
 
@@ -30,8 +29,8 @@ describe Api::V1::SessionController do
       it 'return 400 when email not found' do
         expect(User)
           .to receive(:find_by_email)
-                .with('admin123@admin.com')
-                .and_return(nil)
+          .with('admin123@admin.com')
+          .and_return(nil)
 
         post :new_session, params: request_params
         expect(response.status).eql? 401
