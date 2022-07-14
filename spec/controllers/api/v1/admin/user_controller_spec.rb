c426db9c910377150daa95c68eb67f4583933dd4
koiniin <nkhoinguyen26@gmail.com>
2022-07-14 16:26:38 +0700

    Add test backend step

--- spec/controllers/api/v1/admin/user_controller_spec.rb
+++ spec/controllers/api/v1/admin/user_controller_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe Api::V1::Admin::UsersController do
   describe 'get /users' do
     context 'when request is unauthorized' do
-      expect(authentication)
     end
   end
 end
