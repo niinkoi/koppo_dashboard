3eeb6adf42c8cdf61b1f248685d3a793347b7351
koiniin <nkhoinguyen26@gmail.com>
2022-07-14 16:30:33 +0700

    using pending to avoid empty block

--- spec/controllers/api/v1/admin/user_controller_spec.rb
+++ spec/controllers/api/v1/admin/user_controller_spec.rb
@@ -2,7 +2,6 @@ require 'rails_helper'
 
 describe Api::V1::Admin::UsersController do
   describe 'get /users' do
-    context 'when request is unauthorized' do
-    end
+    pending 'testing request'
   end
 end
